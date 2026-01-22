cb934d1bc982b4a5cdf2f10e1fe44428ca7fea77
Ayusman Nanda <abhiseknanda799@gmail.com>
2026-01-22 23:12:22 +0530

    Add header title and adjust login window size

---
+++ out/production/College_Management_system/README.md
@@ -0,0 +1,6 @@
+# College_Management_system <br>
+Stricture :
+<br>
+src - for source java files <br>
+assets - for images and pictures <br>
+jar - will be used to store jar libraries like jdbc in future <br>
--- src/LoginPageFrame.java
+++ src/LoginPageFrame.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 class LoginPageFrame extends JFrame {
 
     private JPanel contentPane;
+    private JPanel headerPanel;
 
     LoginPageFrame() {
         // Configure the main login window
@@ -19,8 +20,14 @@ class LoginPageFrame extends JFrame {
         // Root container for manual component positioning
         contentPane = new JPanel();
         contentPane.setBackground(Color.WHITE);
-        contentPane.setLayout(null); // required for absolute positioning
+        contentPane.setLayout(null);
         setContentPane(contentPane);
+
+        // Top header area for branding or title
+        headerPanel = new JPanel();
+        headerPanel.setBackground(new Color(39, 71, 122)); // updated header color
+        headerPanel.setBounds(0, 0, 400, 60);
+        contentPane.add(headerPanel);
     }
 
     public static void main(String[] args) {
